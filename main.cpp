ef709fbb730e031bb55cde52197a88e99c3487d0
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-10-28 21:47:30 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//File Name: main.cpp
+//Author: George Gesite
+//Activity 5 CPE 411 Embedded System
+//Started 28/10/22
+//Description: Checksum C++ Implementation Program 
+
+#include <iostream>
+#include <bits/stdc++.h>
+#include "includes/checksum.h"
+using namespace std;
+//to run code
+//g++ -o main.exe main.cpp includes/checksum.cpp
+//./main.exe
+
+// Driver Code
+int main()
+{
+	string sent_message, recv_message;
+	int block_size;
+	 cout<<"Enter Sent Message (In_Binary)"<<endl;
+	 cin>> sent_message;
+	 cout<<"Enter Recieved Message (In_Binary)"<<endl;
+	 cin>> recv_message;
+	 cout<<"Enter number of bits per block/section"<<endl;
+	 cin>> block_size;
+	//10000101011000111001010011101101 example binary/ 8 bits per block
+
+	if (checker(sent_message,
+				recv_message,
+				block_size)) {
+		cout << "No Error";
+	}
+	else {
+		cout << "Error";
+	}
+
+	return 0;
+}
