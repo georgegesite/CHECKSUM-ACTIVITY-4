407aa44e0ae85bdcf5cd5892ec5d4d8f39fd432c
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-10-29 20:35:37 +0800

    Add files via upload

--- includes/crc.cpp
+++ includes/crc.cpp
@@ -5,8 +5,8 @@
 //Description: CRC C++ Implementation Program 
 #include <iostream>
 using namespace std;
-string input,divisor,divident,result;
- int len_divident, len_gen, len_inp;
+string input,divisor,dividend,result;
+ int len_dividend, len_gen, len_inp;
 
 string fun_xor(string a,string b)
     {
@@ -24,79 +24,76 @@ string fun_xor(string a,string b)
         }
     }
 void modulo_div()
+{     
+    string temp_div=divisor;
+    string temp_dividend=dividend.substr(0,len_gen);
+    int j=len_gen;
+    while(j<len_dividend)
     {
-        
-        string temp_div=divisor;
-        string temp_divident=divident.substr(0,len_gen);
-        int j=len_gen;
-        while(j<len_divident)
-        {
-            temp_divident=fun_xor(temp_divident,temp_div);
-            temp_divident=temp_divident+divident[j];
-            j++;
-        }
-        result=input+fun_xor(temp_divident, temp_div);
+        temp_dividend=fun_xor(temp_dividend,temp_div);
+        temp_dividend=temp_dividend+dividend[j];
+        j++;
     }
+    result=input+fun_xor(temp_dividend, temp_div);
+}
 
-    void getdata()
+void getdata()
+{
+    // First ask for the input in binary
+    // Next is ask for the divisor/genarator polynomial
+    cout<<"\nEnter Input: ";
+    cin>>input;
+    cout<<"\nEnter coefficients of generator polynomial: ";
+    cin>>divisor;
+    len_gen=divisor.length();
+    len_inp=input.length();
+    dividend=input;
+    // Int r is the number of zeroes added to the end of the input which will be the new dividend 
+    int r=len_gen-1;
+    for(int i=0;i<r;i++)
     {
-        
-        cout<<"\nEnter Input: ";
-        cin>>input;
-        cout<<"\nEnter coefficients of generator polynomial: ";
-        cin>>divisor;
-        
-        len_gen=divisor.length();
-        len_inp=input.length();
-        divident=input;
-        int r=len_gen-1;
-        for(int i=0;i<r;i++)
-        {
-            divident=divident+'0';      
-        }
-        len_divident=divident.length();
-        
-        modulo_div();   
+        dividend=dividend+'0';      
     }
+    len_dividend=dividend.length();
+    modulo_div();   
+}
 
 void sender_side()
-    {
-        cout<<"Input: "<<input<<"\n";
-        cout<<"Polynomial: "<<divisor<<"\n";
-        cout<<"Divident: "<<divident<<"\n";
-        cout<<"Data to send: "<<result<<"\n";
-        
-    } 
+{
+    cout<<"Input: "<<input<<"\n";
+    cout<<"Polynomial: "<<divisor<<"\n";
+    cout<<"Dividend: "<<dividend<<"\n";
+    cout<<"Data Sent : "<<result<<"\n";
+} 
     
 void receiver_side()
+{
+    string data_rec;
+    cout<<"\nEnter Data Received: ";
+    cin>>data_rec;
+
+    string temp_div=divisor;       
+    string temp_dividend=data_rec.substr(0,len_gen);
+    int j=len_gen;
+    while(j<data_rec.length())
     {
-        string data_rec;
-        cout<<"\nEnter Data Received: ";
-        cin>>data_rec;
-
-        string temp_div=divisor;
-        string temp_divident=data_rec.substr(0,len_gen);
-        int j=len_gen;
-        while(j<data_rec.length())
-        {
-            temp_divident=fun_xor(temp_divident,temp_div);
-            temp_divident=temp_divident+data_rec[j];
-            j++;
-        }
-        string error=fun_xor(temp_divident, temp_div);
-        cout<<"reminder is: "<<error;
-
-        bool flag=0;
-        for(int i=0;i<len_gen-1;i++)
-        {
-            if(error[i]=='1')
-            {
-                flag=1;
-                break;
-            }
-        }
-        if(flag==0)
-        cout<<"\nCorrect Data Received Without Any Error";
-        else
-        cout<<"\nData Received Contain Some  Error";
+        temp_dividend=fun_xor(temp_dividend,temp_div);
+        temp_dividend=temp_dividend+data_rec[j];
+        j++;
     }
+    string error=fun_xor(temp_dividend, temp_div);
+    cout<<"Remainder is: "<<error;
+
+    bool flag=0;
+    for(int i=0;i<len_gen-1;i++)
+    {
+        if(error[i]=='1')
+        {
+            flag=1;
+            break;            }
+        }
+    if(flag==0)
+    cout<<"\nNo Error";
+        else
+    cout<<"\nError";
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-//File Name: main.cpp
-//Author: George Gesite
-//Activity 5 CPE 411 Embedded System
-//Started 28/10/22
-//Description: CRC C++ Implementation Program 
+// File Name: main.cpp
+// Author: George Gesite
+// Activity 5 CPE 411 Embedded System
+// Started 28/10/22
+// Description: CRC C++ Implementation Program 
+
+// To compile this from the command line we would use:
+// g++ -o main.exe main.cpp includes/crc.cpp
+// To run this code in commande line, we use:
+// ./main.exe
+
 #include <iostream>
 #include <bits/stdc++.h>
 #include "includes/crc.h"
@@ -10,7 +16,7 @@ using namespace std;
 
 int main() 
 {   
-    //driver code
+    //Driver Code for Cyclic Redundancy Check
     getdata();
     sender_side();
     receiver_side();
