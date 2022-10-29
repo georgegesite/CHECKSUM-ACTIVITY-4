ac231f37265d85e7c2b5b3321934c1dc8ef5d2c6
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-10-29 19:14:18 +0800

    testing

---
+++ includes/crc.h
@@ -0,0 +1,20 @@
+//File Name: crc.h
+//Author: George Gesite
+//Activity 5 CPE 411 Embedded System
+//Started 28/10/22
+//Description: CRC C++ Implementation Program 
+#ifndef CRC_H
+#define CRC_H
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//enter prototypes
+    string fun_xor(string a,string b);
+    void modulo_div();
+    void getdata();
+    void sender_side();
+    void receiver_side();
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -2,35 +2,17 @@
 //Author: George Gesite
 //Activity 5 CPE 411 Embedded System
 //Started 28/10/22
-//Description: Checksum C++ Implementation Program 
+//Description: CRC C++ Implementation Program 
 #include <iostream>
 #include <bits/stdc++.h>
-#include "includes/checksum.h"
+#include "includes/crc.h"
 using namespace std;
-//to run code
-//g++ -o main.exe main.cpp includes/checksum.cpp
-//./main.exe
 
-// Driver Code
-int main()
-{
-	string sent_message, recv_message;
-	int block_size;
-	 cout<<"Enter Sent Message (In_Binary)"<<endl;
-	 cin>> sent_message;
-	 cout<<"Enter Recieved Message (In_Binary)"<<endl;
-	 cin>> recv_message;
-	 cout<<"Enter number of bits per block/section"<<endl;
-	 cin>> block_size;
-
-	if (checker(sent_message,
-				recv_message,
-				block_size)) {
-		cout << "No Error";
-	}
-	else {
-		cout << "Error";
-	}
-
-	return 0;
+int main() 
+{   
+    //driver code
+    getdata();
+    sender_side();
+    receiver_side();
+    return 0;
 }
