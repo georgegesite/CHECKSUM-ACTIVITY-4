7210bc7f6dd4d98a27af1a0b9072c508f0bd8c41
George Gesite <104239892+georgegesite@users.noreply.github.com>
2022-10-29 20:37:30 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,23 +2,38 @@
 // Author: George Gesite
 // Activity 5 CPE 411 Embedded System
 // Started 28/10/22
-// Description: CRC C++ Implementation Program 
+// Description: Checksum C++ Implementation Program 
 
 // To compile this from the command line we would use:
-// g++ -o main.exe main.cpp includes/crc.cpp
+// g++ -o main.exe main.cpp includes/checksum.cpp
 // To run this code in commande line, we use:
 // ./main.exe
-
 #include <iostream>
 #include <bits/stdc++.h>
-#include "includes/crc.h"
+#include "includes/checksum.h"
 using namespace std;
 
-int main() 
-{   
-    //Driver Code for Cyclic Redundancy Check
-    getdata();
-    sender_side();
-    receiver_side();
-    return 0;
+
+// Driver Code
+int main()
+{
+	string sent_message, recv_message;
+	int block_size;
+	 cout<<"Enter Sent Message (In_Binary)"<<endl;
+	 cin>> sent_message;
+	 cout<<"Enter Recieved Message (In_Binary)"<<endl;
+	 cin>> recv_message;
+	 cout<<"Enter number of bits per block/section"<<endl;
+	 cin>> block_size;
+
+	if (checker(sent_message,
+				recv_message,
+				block_size)) {
+		cout << "No Error";
+	}
+	else {
+		cout << "Error";
+	}
+
+	return 0;
 }
